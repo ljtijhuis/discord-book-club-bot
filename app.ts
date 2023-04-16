cc67dd3bbbd3479b6b743bc935ce540a98497d4c
Lars Tijhuis <ljtijhuis@gmail.com>
2023-04-15 21:19:12 -0700

    Save and load state to/from file.

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import config from './lib/config.js';
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import fs from 'fs';
+import * as fsPromise from 'fs/promises';
+
 import { InteractionType, InteractionResponseType } from 'discord-interactions';
 
 import { VerifyDiscordRequest } from './lib/utils.js';
@@ -15,8 +18,8 @@ const PORT = config.PORT || 3000;
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(config.PUBLIC_KEY) }));
 
-// Some in-memory state for the time being
-const bookClubState: BookClubState = {
+// We simply load state from a file (and write it after each request)
+let bookClubState: BookClubState = {
     shortlist: {
         books: [],
     },
@@ -27,41 +30,70 @@ const bookClubState: BookClubState = {
     events: [],
 };
 
+const stateFileName = './server_state.json';
+
+// Loading the state only when the server boots
+(function () {
+    try {
+        const stateJSON = fs.readFileSync(stateFileName, 'utf-8');
+        bookClubState = JSON.parse(stateJSON);
+    } catch (error) {
+        console.error('Error loading state, starting blank', error);
+    }
+})();
+
+const writeState = async function (
+    _req: Request,
+    _res: Response,
+    next: NextFunction,
+) {
+    const json = JSON.stringify(bookClubState);
+    await fsPromise.writeFile(stateFileName, json);
+    next();
+};
+
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', async function (req: Request, res: Response) {
-    // Interaction type and data
-    const { type, data } = req.body;
+app.post(
+    '/interactions',
+    async function (req: Request, res: Response, next: NextFunction) {
+        // Interaction type and data
+        const { type, data } = req.body;
 
-    /**
-     * Handle verification requests
-     */
-    if (type === InteractionType.PING) {
-        return res.send({ type: InteractionResponseType.PONG });
-    }
+        /**
+         * Handle verification requests
+         */
+        if (type === InteractionType.PING) {
+            res.send({ type: InteractionResponseType.PONG });
+        }
 
-    /**
-     * Handle slash command requests
-     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-     */
-    if (type === InteractionType.APPLICATION_COMMAND) {
-        const { name } = data;
+        /**
+         * Handle slash command requests
+         * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+         */
+        if (type === InteractionType.APPLICATION_COMMAND) {
+            const { name } = data;
 
-        const command = CommandFactory.getCommand(name);
-        return command.execute(req, res, bookClubState);
-    }
+            const command = CommandFactory.getCommand(name);
+            await command.execute(req, res, bookClubState);
+        }
 
-    if (type === InteractionType.MESSAGE_COMPONENT) {
-        // This is used for responses through our message UI components
+        if (type === InteractionType.MESSAGE_COMPONENT) {
+            // This is used for responses through our message UI components
 
-        const { custom_id } = data;
-        const action = MessageActionFactory.getAction(custom_id);
-        return action.execute(req, res, bookClubState);
-    }
+            const { custom_id } = data;
+            const action = MessageActionFactory.getAction(custom_id);
+            await action.execute(req, res, bookClubState);
+        }
 
-    console.warn('Unhandled request');
-});
+        next();
+        if (!res.headersSent) {
+            console.warn('Unhandled request');
+        }
+    },
+    writeState, // When calling next(), we persist the state in a file
+);
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
